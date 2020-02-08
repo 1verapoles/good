8f41da89cb4262d0da9c86a6dba4643124d31016
vera <verapoles1@mail.ru>
2020-02-08 10:05:50 +0300

    project

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function() {
+      $(".owl1").owlCarousel({
+   	items: 1,
+   	loop: true,
+   	nav: true,
+   	dots: true,
+    navText: ["<img src='assets/images/arrow_left.png' alt='left' class='img-fluid' />", "<img src='assets/images/arrow_right.png' alt='right' class='img-fluid' />"]
+        });
+});     
+
+    
